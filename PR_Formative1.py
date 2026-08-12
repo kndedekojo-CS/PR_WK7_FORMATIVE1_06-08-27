2b88ecad6a661230bdfec58758f8b58c77d80413
Korkor Ndede Kojo <k.ndedekojo@alustudent.com>
2026-08-12 05:04:18 +0400

    Addede the filter feature

--- PR_Formative1.py
+++ PR_Formative1.py
@@ -3,6 +3,9 @@
 #ASSIGNMENT CLASS
 #This will be the parent class for all assignments. It will contain information that both homework and exams will have.
 
+from calendar import month
+
+
 class Assignment:
     def __init__(self, subject, title, score, max_score, due_date, assignment_type):
         #This is to store the information about the  assignment, so it can be used later to calculate the grade.
@@ -47,6 +50,12 @@ def get_score(message):
         except:
             print("Please enter a number.")
 
+def show_matches(matches):
+    if len(matches) == 0:
+        print("No assignments found.")
+    else:
+        for assignment in matches:
+            print(assignment.title)
 
 #GRADE TRACKER CLASS
 #The grade tracker class will be used to store all of the assignments and later list,filter and calculate grade for each subject.
@@ -124,3 +133,48 @@ class GradeTracker:
             print(f"Score: {assignment.score}/{assignment.max_score}")
             print(f"Due Date: {assignment.due_date}")
             print(f"Type: {assignment.assignment_type}")
+
+    def filter_assignments_by_subject(self, subject):
+        #This will ask the user how they want to filter the assignments.
+
+        print(f"\n~~~~~~~~~~~~FILTER ASSIGNMENTS~~~~~~~~~~~~")
+
+        print(f"\nHow would you like to filter the assignments?")
+        print(f"1. Filter by subject")
+        print(f"2. Filter by assignment type(Homework or Exam)")
+        print(f"3. Filter by Month(MM/YYYY )")
+
+        choice = input("Enter your choice (1-3): ")
+
+        matches = []
+
+        #Filter assignment by subject
+        if choice == "1":
+            subject = input("Enter the subject to filter by: ")
+            for assignment in self.assignments:
+                if assignment.subject.lower() == subject.lower():
+                    matches.append(assignment)
+
+        #Filter assignment by type
+        elif choice == "2":
+            assignment_type = input("Enter the assignment type to filter by (Homework or Exam): ")
+            for assignment in self.assignments:
+                if assignment.assignment_type.lower() == assignment_type.lower():
+                    matches.append(assignment)
+
+        #Filter assignment by month
+        elif choice == "3":
+            month_year = input("Enter the month and year to filter by (MM/YYYY): ")
+            for assignment in self.assignments:
+                 #due_date looks like 15/08/2026, so characters 3 to 10 are 08/2026
+                if assignment.due_date[3:10] == month:
+                    matches.append(assignment)
+
+        else:
+            print("Invalid choice. Please try again.")
+            return
+
+        #This will display the matches found based on the filter criteria.
+        show_matches(matches)
+
+    
